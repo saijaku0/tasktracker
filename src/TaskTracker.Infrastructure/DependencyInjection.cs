5783bccedcd817a77a20cfd899926c68b2538c35
saijaku0 <minerking112256@gmail.com>
2026-01-10 14:40:52 +0100

    feat: add dependency injection

---
+++ src/TaskTracker.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using TaskTracker.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace TaskTracker.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<ApplicationDbContext>((sp, options) =>
+            {
+                options.UseNpgsql(connectionString);
+            });
+
+            return services;
+        }
+    }
+}
