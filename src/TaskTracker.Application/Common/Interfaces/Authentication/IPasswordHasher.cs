98de09f3bb52bb0fa8597dd5171251d6d3544c8e
saijaku0 <minerking112256@gmail.com>
2026-01-12 12:30:07 +0100

    feat: add password hasher interface

---
+++ src/TaskTracker.Application/Common/Interfaces/Authentication/IPasswordHasher.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskTracker.Application.Users.Commands.Interfaces.Authentication
+{
+    public interface IPasswordHasher
+    {
+        string HashPassword(string password);
+        bool VerifyHashedPassword(string hashedPassword, string providedPassword);
+    }
+}
