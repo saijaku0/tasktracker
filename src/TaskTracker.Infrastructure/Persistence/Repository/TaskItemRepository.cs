1fc8210bc15d4f7ef39f0539ac8458cc4a36bf1c
saijaku0 <minerking112256@gmail.com>
2026-01-12 11:08:13 +0100

    feat: add taskitem repository

---
+++ src/TaskTracker.Infrastructure/Persistence/Repository/TaskItemRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskTracker.Domain.Interface.Repository;
+using TaskTracker.Domain.TasksItem;
+
+namespace TaskTracker.Infrastructure.Persistence.Repository
+{
+    public class TaskItemRepository(ApplicationDbContext context)
+        : ITaskRepository
+    {
+        private readonly ApplicationDbContext _context = context;
+
+        public async Task<TaskItem?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            return await _context.FindAsync<TaskItem>([id], cancellationToken);
+        }
+
+        public async Task AddAsync(TaskItem task, CancellationToken cancellationToken = default)
+        {
+            await _context.AddAsync(task, cancellationToken);
+        }
+
+        public Task UpdateAsync(TaskItem task, CancellationToken cancellationToken = default)
+        {
+            _context.Update(task);
+            return Task.CompletedTask;
+        }
+
+        public async Task<IReadOnlyList<TaskItem>> GetByUserIdAsync(
+            Guid userId,
+            CancellationToken cancellationToken = default)
+        {
+            return await _context
+                .Set<TaskItem>()
+                .Where(t => t.AssignedUserId == userId)
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
