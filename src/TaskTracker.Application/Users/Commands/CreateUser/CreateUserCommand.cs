a35fe5b0717198ccdb5fd225b9662a35c73e3360
saijaku0 <minerking112256@gmail.com>
2026-01-12 12:27:32 +0100

    feat: add user command for create

---
+++ src/TaskTracker.Application/Users/Commands/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace TaskTracker.Application.Users.Commands.CreateUser
+{
+    public record CreateUserCommand(
+        string FirstName,
+        string LastName,
+        string Email,
+        string Password
+    )
+        : IRequest<Guid>;
+}
