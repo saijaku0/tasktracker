920b59d0bfb2dd60807cec7ed9a4b4b3f31764fa
saijaku0 <minerking112256@gmail.com>
2026-01-13 11:58:35 +0100

    feat: add di for user, and task, password hash

--- src/TaskTracker.Infrastructure/DependencyInjection.cs
+++ src/TaskTracker.Infrastructure/DependencyInjection.cs
@@ -1,20 +1,29 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TaskTracker.Application.Common.Interfaces.Authentication;
+using TaskTracker.Domain.Interface.Repository;
 using TaskTracker.Infrastructure.Persistence;
+using TaskTracker.Infrastructure.Persistence.Repository;
+using TaskTracker.Infrastructure.Services;
 
 namespace TaskTracker.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
         {
             var connectionString = configuration.GetConnectionString("DefaultConnection");
 
             services.AddDbContext<ApplicationDbContext>((sp, options) =>
-            {
-                options.UseNpgsql(connectionString);
-            });
+                options.UseNpgsql(connectionString));
+
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<ITaskRepository, TaskItemRepository>();
+
+            services.AddSingleton<IPasswordHasher, PasswordHasher>();
 
             return services;
         }
